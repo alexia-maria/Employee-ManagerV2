6b4605bd00ebcd748a2c31fd585656e2f8b67523
alexia-maria <115625535+alexia-maria@users.noreply.github.com>
2024-10-29 14:58:03 -0300

    Delete SpringAngular directory

--- SpringAngular/src/main/java/com/example/SpringAngular/persistence/repository/UserrRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SpringAngular.persistence.repository;
-
-import com.example.SpringAngular.persistence.model.Userr;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserrRepository extends CrudRepository<Userr, Long> {
-}
