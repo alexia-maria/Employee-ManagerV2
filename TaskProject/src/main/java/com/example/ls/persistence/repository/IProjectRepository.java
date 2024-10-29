c5cd97d3f75be2acebbe376a57fa83f5c5088302
alexia-maria <115625535+alexia-maria@users.noreply.github.com>
2024-10-29 14:58:24 -0300

    Delete TaskProject directory

--- TaskProject/src/main/java/com/example/ls/persistence/repository/IProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ls.persistence.repository;
-
-import com.example.ls.persistence.model.Project;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface IProjectRepository extends PagingAndSortingRepository<Project, Long> {
-}
--- TaskProject/src/main/java/com/example/ls/persistence/repository/ITaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ls.persistence.repository;
-
-import com.example.ls.persistence.model.Task;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ITaskRepository extends CrudRepository<Task, Long> {
-}
